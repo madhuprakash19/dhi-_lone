80a36f855b026d44344d3f89808fed882df81d0e
madhuprakash19 <madhutp11@gmail.com>
2021-09-02 11:53:51 +0530

    student join and leave feature

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('class/',views.ListClass.as_view(),name='all_class'),
     path('class/new/',views.CreatClassView.as_view(),name='new_class'),
     path('class/single/(?P<slug>[-\w]+)/',views.SingleClass.as_view(),name='single_class'),
+    path('class/join/(?P<slug>[-\w]+)/',views.JoinClass.as_view(),name='join'),
+    path('class/leave/(?P<slug>[-\w]+)/',views.LeaveClass.as_view(),name='leave'),
 
 ]
--- user/views.py
+++ user/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views.generic import (TemplateView,DetailView,
                                 CreateView,DetailView,
-                                ListView,)
+                                ListView,RedirectView)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from accounts.models import UserProfileInfo
-from . import models
+from user.models import CreateClass,ClassMember
+from django.contrib import messages
 
 
 class HomePage(LoginRequiredMixin,TemplateView):
@@ -15,24 +17,57 @@ class HomePage(LoginRequiredMixin,TemplateView):
 
 class CreatClassView(LoginRequiredMixin,CreateView):
     fields=('sem','subject','subject_code')
-    model = models.CreateClass
+    model = CreateClass
     context_object_name = 'class'
     template_name = 'user/class_form.html'
     success_url = '/user/class'
 
     def form_valid(self,form):
-
         classroom = form.save(commit=False)
         classroom.teacher = self.request.user
         classroom = classroom.save()
         return HttpResponseRedirect(self.success_url)
 
 class SingleClass(LoginRequiredMixin,DetailView):
-    model = models.CreateClass
+    model = CreateClass
     context_object_name = 'class'
     template_name = 'user/class_detail.html'
 
 class ListClass(LoginRequiredMixin,ListView):
-    model = models.CreateClass
+    model = CreateClass
     context_object_name = 'class'
     template_name = 'user/class_list.html'
+
+class JoinClass(LoginRequiredMixin,RedirectView):
+
+    def get_redirect_url(self,*args,**kwargs):
+        return reverse('user:single_class',kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self,request,*args,**kwargs):
+        classroom = get_object_or_404(CreateClass,slug=self.kwargs.get('slug'))
+
+        try:
+            ClassMember.objects.create(user=self.request.user,class_group=classroom)
+        except IntegrityError:
+            messages.warning(self.request,'Warning already a student of the class!')
+        else:
+            messages.success(self.request,'You are now a student of this class!')
+
+        return super().get(request,*args,**kwargs)
+
+class LeaveClass(LoginRequiredMixin,RedirectView):
+    def get_redirect_url(self,*args,**kwargs):
+        return reverse('user:single_class',kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self,request,*args,**kwargs):
+        try:
+            membership = ClassMember.objects.filter(
+            user = self.request.user,
+            class_group__slug = self.kwargs.get('slug')
+            ).get()
+        except ClassMember.DoesNotExist:
+            messages.warning(self.request,'Sorry you are not in this class!')
+        else:
+            membership.delete()
+            messages.success(self.request,'You hav left the class!')
+        return super().get(request,*args,**kwargs)
